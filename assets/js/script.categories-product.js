e9ba69e24d2befce90b2384bc5d87b88d9fbb5b6
Alineevang <aline.evangelista@aluno.senai.br>
2023-08-17 10:17:59 -0300

    feat:add class Products

--- assets/js/script.categories-product.js
+++ assets/js/script.categories-product.js
@@ -6,3 +6,13 @@ class Category {
         this.products = [];
     }
 }
+
+//produto pertence a uma categoria => 1:1
+class Products {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+}
+}
