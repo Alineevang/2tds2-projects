be1278331c676c94cfd94476c778677452914a33
Alineevang <aline.evangelista@aluno.senai.br>
2023-08-17 10:31:55 -0300

    feat: create Class add category and category service

--- assets/js/script.categories-product.js
+++ assets/js/script.categories-product.js
@@ -14,5 +14,18 @@ class Products {
         this.name = name;
         this.price = price;
         this.category = category;
+    }
 }
+
+class CategoryService {
+    constructor() {
+        this.categories = [];
+        this.nextCategoryId = 0;
+    }
+
+    addCategory(name) {
+        const id = this.nextCategoryId ++;
+        const category = new Category(id, name);
+        this.categories.push(category);
+    }
 }
